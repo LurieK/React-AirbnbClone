88c493d9a49e78d8a45bb2ee1560c899a33a9d2a
Lurie <lurainekimmerle@gmail.com>
2023-08-15 11:01:05 +0000

    set up Hero Component

--- src/Hero.js
+++ src/Hero.js
@@ -1,1 +1,16 @@
 import Grid from "./photo-grid"
+
+function Hero (){
+    return(
+        <>
+        <div>
+            <img src={Grid} className="grid"/>
+        </div>
+        <div className="hero-text">
+            <h1>Online Experiences</h1>
+            <h3>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h3>
+        </div>
+        </>
+    )
+
+}
