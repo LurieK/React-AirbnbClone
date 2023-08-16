0265e57f1db78f6b6ea572e272d81aa5a5d55ad7
Lurie <lurainekimmerle@gmail.com>
2023-08-16 01:25:37 +0000

    aligned elements in cards flex-start and in a column

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ function Card(){
             
             <div className="card-info">
                 <div className="star">
-                    <img src={Star}/>
+                    <img className="star-img" src={Star}/>
                     <p>5.0(6)-USA</p>
                 </div>
                 <p>Life lessons with Katie Zaferes</p>
