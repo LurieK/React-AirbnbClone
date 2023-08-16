29b9eabb0a2b2ef81effc7d438aca6e121d12443
Lurie <lurainekimmerle@gmail.com>
2023-08-16 01:17:10 +0000

    set width, and flex box for cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
-import Navbar from "./Navbar.js"
-import Hero from "./Hero.js"
+import Navbar from "./Navbar.js";
+import Hero from "./Hero.js";
+import Card from "./Card.js";
 
 export default function App() {
   return (
     <div className="App">
      <Navbar/>
      <Hero/>
+     <Card/>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,23 @@
+import Katie from "./img/Katie_Zaferes.png";
+import Star from "./img/Star 1.png";
+
 function Card(){
     return (
-        
+        <section className="cards">
+            <img className='card-image' src={Katie}/>
+            
+            <div className="card-info">
+                <div className="star">
+                    <img src={Star}/>
+                    <p>5.0(6)-USA</p>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p>From $136 / person</p>
+            </div>
+        </section>
 
     )
 
 }
+
+ export default Card
