c2723b641e7252bd630cf42051cc6c2b3c134c43
Lurie <lurainekimmerle@gmail.com>
2023-08-17 06:51:26 +0000

    fixed if statement with error so Online displays in badge

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,15 @@
 import Star from "./img/Star 1.png";
  
 function Card(props){
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return (
         <section className="cards">
-            {props.openSpots === 0 && <div className="card-badge">SOLD OUT</div>}
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <img className='card-image' src={props.img}/>
             <div className="card-info">
                 <div className="star">
