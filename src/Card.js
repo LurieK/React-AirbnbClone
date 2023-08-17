e4f1b75e6ebd031f6ba61d697927cae20ab7a2a1
Lurie <lurainekimmerle@gmail.com>
2023-08-17 07:13:45 +0000

    pass in item to simplify JSX in App.js

--- src/Card.js
+++ src/Card.js
@@ -3,24 +3,24 @@ import Star from "./img/Star 1.png";
  
 function Card(props){
     let badgeText
-    if (props.openSpots === 0){
+    if (props.item.openSpots === 0){
         badgeText = "SOLD OUT"
-    }else if (props.location === "Online"){
+    }else if (props.item.location === "Online"){
         badgeText = "ONLINE"
     }
     return (
         <section className="cards">
             {badgeText && <div className="card-badge">{badgeText}</div>}
-            <img className='card-image' src={props.img}/>
+            <img className='card-image' src={props.item.coverImg}/>
             <div className="card-info">
                 <div className="star">
                     <img className="star-img" src={Star}/>
                     <span>{props.rating} </span>
-                    <span class="gray"> ({props.reviewCount}) • </span>
-                    <span class="gray">{props.location}</span>
+                    <span class="gray"> ({props.item.stats.reviewCount}) • </span>
+                    <span class="gray">{props.item.stats.location}</span>
                 </div>
-                <p>{props.title}</p>
-                <p><span className="bold">From ${props.price}</span> / person</p>
+                <p>{props.item.title}</p>
+                <p><span className="bold">From ${props.item.price}</span> / person</p>
             </div>
         </section>
 
